969032f00a1df741bff248b38bb3df758c00cace
Carl Berg <carl@carl-berg.se>
2024-03-22 12:13:40 +0100

    Unique db names for the two test suites

--- ADatabaseMigrator.Tests.GalacticWasteManagement/Core/DatabaseFixture.cs
+++ ADatabaseMigrator.Tests.GalacticWasteManagement/Core/DatabaseFixture.cs
@@ -2,4 +2,4 @@
 
 namespace ADatabaseMigrator.Tests.GalacticWasteManagement.Core;
 
-public class DatabaseFixture() : DatabaseFixtureBase(new SqlServerDatabaseAdapter()), IAsyncLifetime { }
+public class DatabaseFixture() : DatabaseFixtureBase(new SqlServerDatabaseAdapter($"ADatabaseMigrator_Tests_GWM_{DateTime.Now:yyyy-MM-dd_HH-mm}")), IAsyncLifetime { }
